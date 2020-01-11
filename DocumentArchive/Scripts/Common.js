c42c26f993c9665327c04b1d94e141de9e174374
Jarosław Gosławski <jarek93pl@hotmail.com>
2020-01-11 17:19:30 +0100

    Dodanie do Wyszukiwania po dacie

--- DocumentArchive/Scripts/Common.js
+++ DocumentArchive/Scripts/Common.js
@@ -19,3 +19,24 @@
     });
 
 }
+function PostActionByString(url, data, completed) {
+    $.ajax({
+        url: url,
+        type: "POST",
+        cache: false,
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            if (data === "") {
+                completed(undefined);
+            }
+            else {
+                completed(data);
+            }
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+
+}
--- DocumentArchive/Scripts/Index.js
+++ DocumentArchive/Scripts/Index.js
@@ -6,9 +6,22 @@
         function (data) {
             var txt = '';
             for (var i in data) {
-                txt += "<tr><td>" + i.DateCreated + "</td>" + Name + "<td></td><td>" + Owner + "</td><td>" + Category+"</td> </tr>";
+                txt += "<tr><td>" + i.DateCreated + "</td>" + Name + "<td></td><td>" + Owner + "</td><td>" + Category + "</td> </tr>";
             }
         }
     );
 }
 LoadDocuments();
+$('#AutorId').bind('input',
+    function () {
+        PostActionByString("/Home/FindAutor", $('#AutorIdbrowsers').val()
+            , function (data) {
+                var optionsHtml = '';
+                for (var i of data) {
+                    var name = i.id + ')'+ (i.firstName === undefined ? "" : i.firstName) + ' ' + (i.lastName === undefined ? "" : i.lastName);
+                    optionsHtml += '<option data-id"' + i.id + '" value="' + name + '"/>';
+                }
+                $('#AutorIdbrowsers').html(optionsHtml);
+            });
+    });
+
